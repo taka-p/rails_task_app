1b7f04da4258832c90d83e4a2a9ec69f59918b9a
TakashiSato <takashisato.235711@gmail.com>
2015-09-19 17:44:36 +0900

    refs finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  resources :projects do
+    resources :tasks, only: [:create, :destroy]
+  end
+
+  post '/projects/:project_id/tasks/:id/toggle' => 'tasks#toggle'
+
+  root 'projects#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
